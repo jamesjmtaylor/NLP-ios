6066ddbe8dfa18a52f8131fff623d1f9523c530e
James Taylor <james.jm.taylor@gmail.com>
2017-06-29 13:45:30 -0700

    Initial Commit

---
+++ nlpSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nlpSpike
+//
+//  Created by Taylor, James on 6/29/17.
+//  Copyright © 2017 Taylor, James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
